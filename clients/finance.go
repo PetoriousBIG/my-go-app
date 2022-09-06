30992923ff61c931a24a7896812c09c5bca22295
PetoriousBIG <peter.m.martin.1993@gmail.com>
2022-09-05 20:07:07 -0400

    feat: enable financal get request

--- clients/finance.go
+++ clients/finance.go
@@ -1,12 +1,39 @@
 package clients
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 type finance struct {
+	BaseCurrency string
 }
 
-func NewFinacne() *finance {
-	return &finance{}
+func NewFinacne(bc string) *finance {
+	return &finance{bc}
 }
 
-func (f *finance) GET() {
+func (f *finance) GET() map[string]interface{} {
+	client := http.Client{}
+	u := "https://api.exchangerate.host/latest"
+	request, err := http.NewRequest("GET", u, nil)
+	values := request.URL.Query()
+	values.Add("base", f.BaseCurrency)
+	request.URL.RawQuery = values.Encode()
+	request.Header.Add("Accept", "application/json")
 
+	fmt.Printf("%v", request.URL)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var result map[string]interface{}
+	json.NewDecoder(resp.Body).Decode(&result)
+	return result
 }
--- handlers/get.go
+++ handlers/get.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 
+	"github.com/PetoriousBIG/my-go-app/clients"
 	"github.com/PetoriousBIG/my-go-app/data"
 	"github.com/PetoriousBIG/my-go-app/util"
 )
@@ -19,29 +19,30 @@ func NewCountryData(l *log.Logger) *countryData {
 }
 
 func (c *countryData) GetCountryData(rw http.ResponseWriter, r *http.Request) {
-	v := fmt.Sprintf("%v", r.Context().Value("valid"))
-	cd := r.Context().Value("header")
+	valid := r.Context().Value("valid").(bool)
+	ch := r.Context().Value("header").(data.CountryHeader)
 
 	rw.Header().Add("Content-Type", "application/json")
 
 	var response any
 
-	isValid, err := strconv.ParseBool(v)
-	if err != nil {
-		c.l.Println("[ERROR] parsing boolean", err)
-		rw.WriteHeader(http.StatusInternalServerError)
-		response = data.ApiError{"internal error"}
-	}
-
-	if isValid {
-		c.l.Println("[DEBUG] getting country data", cd)
+	if valid {
+		c.l.Println("[DEBUG] getting country data", ch)
 		rw.WriteHeader(http.StatusOK)
-		response = cd
+		response = apiMashup(ch)
 	} else {
-		c.l.Println("[DEBUG] country not found", cd)
+		c.l.Println("[DEBUG] country not found", ch)
 		rw.WriteHeader(http.StatusNotFound)
-		response = data.ApiError{fmt.Sprintf("country not found, %v", cd)}
+		response = data.ApiError{fmt.Sprintf("country not found, %v", ch)}
 	}
 
 	util.ToJSON(response, rw)
+}
+
+func apiMashup(ch data.CountryHeader) any {
+	base := ch.Key
+	finance := clients.NewFinacne(base)
+	retval := finance.GET()
+	fmt.Printf("%v", retval)
+	return nil
 }
